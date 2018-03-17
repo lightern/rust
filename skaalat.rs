5709052c891a9a522d17624a4ab8713b28db15c6
Light3rn <31665284+lightern@users.noreply.github.com>
2018-03-17 11:10:39 +0200

    Update skaalat.rs

--- skaalat.rs
+++ skaalat.rs
@@ -58,7 +58,7 @@ fn main() {
 	//soundnumbers.push(soundnumbers[0].unwrap());
 
 	for z in 0..hitvec.len() {
-		println!("{} yhteistä: {} {}, {} {}, {} {}, {} {}, {} {}, {} {}, {} {}", hitvec[mutate(z, 12)], sounds[mutate(z, 12)], scales[0], sounds[mutate(z+2, 12)], scales[1], sounds[mutate(z+4, 12)], scales[1], sounds[mutate(z+5, 12)], scales[1], sounds[mutate(z+7, 12)], scales[1], sounds[mutate(z+9, 12)], scales[1], sounds[mutate(z+2, 11)], scales[1]);
+		println!("{} yhteistä: {} {}, {} {}, {} {}, {} {}, {} {}, {} {}, {} {}", hitvec[mutate(z, 12)], sounds[mutate(z, 12)], scales[0], sounds[mutate(z+2, 12)], scales[1], sounds[mutate(z+4, 12)], scales[1], sounds[mutate(z+5, 12)], scales[1], sounds[mutate(z+7, 12)], scales[1], sounds[mutate(z+9, 12)], scales[1], sounds[mutate(z+11, 12)], scales[1]);
 		}
 pause();
 }
